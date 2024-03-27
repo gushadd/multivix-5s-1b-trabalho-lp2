13ca2dae07884f03c200175ee6894f7b1f6dd08c
gPagio <guiguipagio@gmail.com>
2024-03-26 21:42:00 -0300

    feat: propriedades contato
    Cria getters e setters com validacao para os campos do contato

--- Models/Contato.cs
+++ Models/Contato.cs
@@ -1,9 +1,54 @@
-﻿namespace Gestor_de_Eventos.Models;
+﻿using System.Text.RegularExpressions;
+
+namespace Gestor_de_Eventos.Models;
 
 public class Contato
 {
-    public string? Cpf { get; set; } 
-    public string? Nome { get; set; }
-    public string? Telefone { get; set; }
-    public string? Email { get; set; }
+    private string? cpf;
+    public string? Cpf
+    {
+        get { return cpf; }
+        set
+        {
+            if (value == null) throw new ArgumentNullException("CPF não pode ser nulo");
+            if (value.Length != 11) throw new ArgumentException("CPF deve ter exatamente 11 caracteres");
+            if (!Regex.IsMatch(value, "^[0-9]+$")) throw new ArgumentException("CPF deve conter apenas números");
+            cpf = value;
+        }    
+    }
+
+    private string? nome;
+    public string? Nome 
+    {
+        get { return nome; }
+        set
+        {
+            if (value == null) throw new ArgumentNullException("Nome não pode ser nulo");
+            if (!Regex.IsMatch(value, @"^[\p{L}\s]+$")) throw new ArgumentException("Nome deve conter apenas caracteres de letras");
+            nome = value;
+        }
+    }
+
+    private string? telefone;
+    public string? Telefone 
+    {
+        get { return telefone; }
+        set
+        {
+            if (value.Length != 11) throw new ArgumentException("Telefone deve ter exatamente 11 caracteres");
+            if (!Regex.IsMatch(value, @"^[0-9]+$")) throw new ArgumentException("Telefone deve conter apenas números");
+            telefone = value;
+        }
+    }
+
+    private string? email;
+    public string? Email 
+    {
+        get { return email; }
+        set
+        {
+            if (!Regex.IsMatch(value, @"^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$")) throw new ArgumentException("O email deve possuir o formato exemplo@dominio.com");
+            email = value;
+        }
+    }
 }
--- obj/Debug/net8.0/Gestor de Eventos.AssemblyInfo.cs
+++ obj/Debug/net8.0/Gestor de Eventos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Gestor de Eventos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83d17d63f2c1423553970a5b0a3eb9c066bd9fa2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Gestor de Eventos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Gestor de Eventos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
