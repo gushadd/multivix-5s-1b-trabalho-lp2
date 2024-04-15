71832914c8444ee9dcb525537dd4a70fadd91858
gPagio <guilhermepagio.contato@gmail.com>
2024-04-15 16:18:01 -0300

    feat: first commit
    Classes de patterns e de validacoes as quais centralizarão todos as validações da aplicação

---
+++ Util/Input/InputValidator.cs
@@ -0,0 +1,36 @@
+﻿using Gestor_de_Eventos.Util.Patterns;
+using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace Gestor_de_Eventos.Util.Input
+{
+    internal class InputValidator
+    {
+        private InputValidator() { }
+
+        public static bool ValidaFormatoDataHoraMinutoBrasileiro(DateTime dataHora)
+        {
+            return DateTime.TryParseExact(dataHora.ToString(ValidationPatterns.FormatoDataHoraMinutoBrasileiro), ValidationPatterns.FormatoDataHoraMinutoBrasileiro, CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime data);
+        }
+
+        public static bool ValidaFormatoEmail(string email)
+        {
+            return Regex.IsMatch(email, ValidationPatterns.FormatoEmail);
+        }
+
+        public static bool ContemApenasNumerosInteiros(int numeroInteiro)
+        {
+            return Regex.IsMatch(numeroInteiro.ToString(), ValidationPatterns.FormatoNumerosInteiros);
+        }
+
+        public static bool ContemApenasNumerosInteiros(string numeroInteiro)
+        {
+            return Regex.IsMatch(numeroInteiro.ToString(), ValidationPatterns.FormatoNumerosInteiros);
+        }
+
+        public static bool ContemApenasLetras(string texto)
+        {
+            return Regex.IsMatch(texto, ValidationPatterns.FormatoLetras);
+        }
+    }
+}
---
+++ Util/Patterns/ValidationPatterns.cs
@@ -0,0 +1,13 @@
+﻿namespace Gestor_de_Eventos.Util.Patterns
+{
+    internal class ValidationPatterns
+    {
+        public const string FormatoDataHoraMinutoBrasileiro = "dd/MM/yyyy HH:mm";
+
+        public const string FormatoEmail = @"^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$";
+
+        public const string FormatoNumerosInteiros = @"^[0-9]+$";
+
+        public const string FormatoLetras = @"^[\p{L}\s]+$";
+    }
+}
