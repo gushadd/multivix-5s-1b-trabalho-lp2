9c03ea4bd65c34fea4713322510fdd72e55d2481
gPagio <guilhermepagio.contato@gmail.com>
2024-04-15 17:16:19 -0300

    rafactor: quebra data em formato e mascara
    Mascara agora compete ao formato usado em formatações visuais. Já formato, compete a uma especificação que uma informação precisa se enquadrar, literalmente uma forma

--- Util/Patterns/ValidationPatterns.cs
+++ Util/Patterns/ValidationPatterns.cs
@@ -2,7 +2,9 @@
 {
     internal class ValidationPatterns
     {
-        public const string FormatoDataHoraMinutoBrasileiro = "dd/MM/yyyy HH:mm";
+        public const string MascaraDataHoraMinutoBrasileiro = "dd/MM/yyyy HH:mm";
+
+        public const string FormatoDataHoraMinutoBrasileiro = @"^(0[1-9]|[12][0-9]|3[01])/(0[1-9]|1[012])/((19|20)\d\d) ([01][0-9]|2[0-3]):[0-5][0-9]$";
 
         public const string FormatoEmail = @"^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$";
 
