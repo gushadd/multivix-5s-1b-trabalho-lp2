88ffbdf9f44b1357e6687613b0feae1e017f3072
gPagio <guilhermepagio.contato@gmail.com>
2024-04-15 20:40:18 -0300

    feat: nova mascara e novo formato
    Mascara e formato para data apenas

--- Util/Patterns/ValidationPatterns.cs
+++ Util/Patterns/ValidationPatterns.cs
@@ -2,7 +2,11 @@
 {
     internal class ValidationPatterns
     {
+        public const string MascaraDataBrasileira = "dd/MM/yyyy";
+
         public const string MascaraDataHoraMinutoBrasileiro = "dd/MM/yyyy HH:mm";
+
+        public const string FormatoDataBrasileira = @"^(0[1-9]|[12][0-9]|3[01])/(0[1-9]|1[012])/((19|20)\d\d)$";
 
         public const string FormatoDataHoraMinutoBrasileiro = @"^(0[1-9]|[12][0-9]|3[01])/(0[1-9]|1[012])/((19|20)\d\d) ([01][0-9]|2[0-3]):[0-5][0-9]$";
 
