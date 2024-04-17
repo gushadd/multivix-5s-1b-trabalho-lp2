4420947a8863dd75bb8ecfb2b57754b6ea5f03f9
gPagio <guilhermepagio.contato@gmail.com>
2024-04-17 15:12:21 -0300

    feat: first commit
    Classe responsavel por exportar eventos

---
+++ Util/Output/ExportarEventos.cs
@@ -0,0 +1,35 @@
+﻿using Gestor_de_Eventos.Models;
+
+namespace Gestor_de_Eventos.Util.Output
+{
+    internal class ExportarEventos
+    {
+        private ExportarEventos() { }
+
+        public static void SalvarArquivoTxt(Evento evento)
+        {
+            string diretorioExportacao = "C:\\gerenciador-eventos-mega-blaster-power-2000";
+
+            if (!Directory.Exists(diretorioExportacao))
+            {
+                Directory.CreateDirectory(diretorioExportacao);
+            }
+
+            string caminhoDoArquivo = Path.Combine(diretorioExportacao, "evento-" + evento.Id + ".txt");
+            StreamWriter sw = new StreamWriter(caminhoDoArquivo, true);
+
+            try
+            {
+                sw.WriteLine(evento.ToString());
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Não foi possível exportar os eventos para o arquivo .txt!");
+            }
+            finally
+            {
+                sw.Close();
+            }
+        }
+    }
+}
