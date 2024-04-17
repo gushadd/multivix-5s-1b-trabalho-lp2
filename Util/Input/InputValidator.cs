fda10d998569fad6e0978324229565906ca34685
gPagio <guilhermepagio.contato@gmail.com>
2024-04-16 21:41:02 -0300

    feat: metodo valida periodo
    Metodo para validar periodos DateTime

--- Util/Input/InputValidator.cs
+++ Util/Input/InputValidator.cs
@@ -45,14 +45,14 @@ namespace Gestor_de_Eventos.Util.Input
             return ValidaFormatoDataHoraMinutoBrasileiro(dataHora.ToString(ValidationPatterns.MascaraDataHoraMinutoBrasileiro));
         }
 
-        public static bool ValidaFormatoDataBrasileira(DateOnly data)
-        {
-            return Regex.IsMatch(data.ToString(ValidationPatterns.MascaraDataBrasileira), ValidationPatterns.FormatoDataHoraMinutoBrasileiro);
-        }
-
         public static bool ValidaFormatoDataBrasileira(string data)
         {
             return Regex.IsMatch(data, ValidationPatterns.FormatoDataBrasileira);
+        }
+
+        public static bool ValidaFormatoDataBrasileira(DateOnly data)
+        {
+            return Regex.IsMatch(data.ToString(ValidationPatterns.MascaraDataBrasileira), ValidationPatterns.FormatoDataHoraMinutoBrasileiro);
         }
 
         public static bool ValidaFormatoEmail(string email)
@@ -86,6 +86,29 @@ namespace Gestor_de_Eventos.Util.Input
             {
                 return false;
             } else  if (!Regex.IsMatch(telefone, ValidationPatterns.FormatoTelefoneBrasileiroSemCodigoInternacional))
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public static bool ValidaPeriodoDateTime(string dataInicial, DateTime dataFinal)
+        {
+            return ValidaPeriodoDateTime(DateTime.ParseExact(dataInicial, ValidationPatterns.MascaraDataHoraMinutoBrasileiro, null), dataFinal);
+        }
+
+        public static bool ValidaPeriodoDateTime(DateTime dataInicial, DateTime dataFinal)
+        {
+            if (!ValidaFormatoDataHoraMinutoBrasileiro(dataInicial))
+            {
+                return false;
+            }
+            else if (!ValidaFormatoDataHoraMinutoBrasileiro(dataFinal))
+            {
+                return false;
+            }
+            else if (dataFinal.CompareTo(dataInicial) > 0)
             {
                 return false;
             }
